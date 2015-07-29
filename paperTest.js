583c9fbf08013868f15200283169e1cb6d6525cb
Lucas Van Meter <lucasvm@uw.edu>
2015-07-28 17:54:50 -0700

    2nd commit
    yay!

--- paperTest.js
+++ paperTest.js
@@ -1,18 +1,21 @@
 circle = new Path.Circle({
 	center: [20, 20],
 	radius: 17.5,
-	strokeColor: 'black'
+	strokeColor: 'black',
+	fillColor: 'green'
 })
 
 t = 0
+rate = 1/(20*Math.PI)
 curr = circle.position
 
 trace = new Path()
+trace.selected = false
 trace.strokeColor = 'black'
 
 function onFrame(event) {
-	circle.position = view.center+[t/10*Math.cos(t/30),-t/10*Math.sin(t/30)]
+	circle.position = view.center+[t/20*Math.cos(t*rate),-t/20*Math.sin(t*rate)]
 	curr = circle.position
 	trace.add(curr)
-	t++
+	t += 1
 }
